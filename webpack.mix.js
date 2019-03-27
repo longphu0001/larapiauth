f10e5fe5e6ec1c201759dd02ae0df72f97174d60
Nghia Le <nghia.le@alpha-connection.net>
2019-03-27 15:19:30 +0900

    Integrated CoreUI

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,8 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
+const path          = require('path')
+const { version }   = require('./package.json')
+const webpack       = require('webpack')
+const WebpackBar    = require('webpackbar')
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +17,64 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+ devServer: { disableHostCheck: true },
+ resolve  : {
+   alias: {
+     '@'         : path.resolve(__dirname, 'resources/js/coreui/'),
+     'static'    : path.resolve(__dirname, 'resources/static/'),
+     'validators': 'vuelidate/lib/validators',
+   },
+ },
+ plugins: [
+   new WebpackBar({ profile: true }),
+   new webpack.DefinePlugin({ __VERSION: JSON.stringify(version) }),
+ ],
+})
+
+mix.extend('vueOptions', (webpackConfig, vueOptions, ...args) => {
+ const vueLoader = webpackConfig.module.rules.find((loader) => loader.loader === 'vue-loader')
+
+ vueLoader.options = require('webpack-merge').smart(vueLoader.options, vueOptions)
+})
+
+mix.vueOptions({
+ transformToRequire: {
+   'img'             : 'src',
+   'image'           : 'xlink:href',
+   'b-img'           : 'src',
+   'b-img-lazy'      : ['src', 'blank-src'],
+   'b-card'          : 'img-src',
+   'b-card-img'      : 'img-src',
+   'b-carousel-slide': 'img-src',
+   'b-embed'         : 'src',
+ },
+})
+
+mix.extract([
+ 'axios',
+ 'bootstrap',
+ 'bootstrap-vue',
+ 'chart.js',
+ 'jquery',
+ 'lodash',
+ 'moment',
+ 'popper.js',
+ 'select2',
+ 'vue',
+ 'vue-chartjs',
+ 'vue-loading-spinner',
+ 'vue-notification',
+ 'vue-router',
+ 'vue-sweetalert2',
+ 'vuejs-datepicker',
+ 'vuelidate',
+ 'vuex',
+ 'vuex-easy-access',
+])
+
+if (mix.inProduction())
+  mix.version()
+else
+  mix.sourceMaps()
