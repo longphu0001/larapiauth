6c4aa0d47fb176038eafa6dd66bce067bc1950e8
DESKTOP-9LAG54H\Sten <stenle2005@gmail.com>
2019-03-31 22:39:24 +0900

    Empty Admin panel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,10 +15,6 @@
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
 </head>
